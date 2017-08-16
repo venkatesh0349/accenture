c23ecaefe209b672f5e94cda8b0a3b1bd4d4b294
venkatesh0349 <reddy.venkatesh87@gmail.com>
2017-08-16 22:30:05 +0530

    build a new application

---
+++ accenture/src/main/java/com/accenture/App.java
@@ -0,0 +1,13 @@
+package com.accenture;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
